beb57a695661104ea046d379b89d10b64d48d8cd
Michael Eaton <mikeye2012@gmail.com>
2018-07-25 00:26:29 -0700

    BE03 Added open door actor component

--- Source/Udemy_EscapeTheRoom1/OpenDoor.cpp
+++ Source/Udemy_EscapeTheRoom1/OpenDoor.cpp
@@ -1,11 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "PositionReporter.h"
-#include "GameFramework/Actor.h"
+#include "OpenDoor.h"
 
 
 // Sets default values for this component's properties
-UPositionReporter::UPositionReporter()
+UOpenDoor::UOpenDoor()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
@@ -16,19 +15,17 @@ UPositionReporter::UPositionReporter()
 
 
 // Called when the game starts
-void UPositionReporter::BeginPlay()
+void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
 
-	FString objectname = GetOwner()->GetName();
-	FString objectLocation = GetOwner()->GetActorLocation().ToString();
-	UE_LOG(LogTemp, Warning, TEXT("Reported: %s : Location: %s"), *objectname, *objectLocation)
+	// ...
 	
 }
 
 
 // Called every frame
-void UPositionReporter::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
